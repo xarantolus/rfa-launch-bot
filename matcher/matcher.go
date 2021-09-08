3d0d678053df66dc0616ce2d31d844da95fdc999
xarantolus <xarantolus@protonmail.com>
2021-09-08 17:45:15 +0200

    Matcher: ignore tweets older than 24 hours

--- matcher/matcher.go
+++ matcher/matcher.go
@@ -1,14 +1,18 @@
 package matcher
 
 import (
+	"time"
+
 	"github.com/xarantolus/rfa-launch-bot/bot"
 	"github.com/xarantolus/rfa-launch-bot/collector"
+	"github.com/xarantolus/rfa-launch-bot/util"
 )
 
 type Matcher struct {
 	IgnoredUsers *bot.UserList
 
-	seenTweets map[int64]bool
+	seenTweets  map[int64]bool
+	maxTweetAge time.Duration
 
 	importantUsers []string
 
@@ -20,6 +24,8 @@ func NewMatcher(ignoredUsers *bot.UserList) (m *Matcher) {
 	m = &Matcher{
 		IgnoredUsers: ignoredUsers,
 		seenTweets:   make(map[int64]bool),
+
+		maxTweetAge: 24 * time.Hour,
 
 		importantUsers: []string{
 			"rfa_space",
@@ -40,6 +46,11 @@ func (m *Matcher) Match(tweet collector.TweetWrapper) bool {
 		return false
 	}
 	m.seenTweets[tweet.ID] = true
+
+	t, terr := tweet.CreatedAtTime()
+	if util.LogError(terr, "parsing tweet date") || time.Since(t) > m.maxTweetAge {
+		return false
+	}
 
 	// Anything from important accounts should be retweeted
 	if tweet.User != nil && containsStringCaseInsensitive(m.importantUsers, tweet.User.ScreenName) {
