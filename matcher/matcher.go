a048d91479d2ccb14d5a24c454cbfa8a6a9e9dff
xarantolus <xarantolus@protonmail.com>
2021-09-08 21:36:13 +0200

    Keywords

--- matcher/matcher.go
+++ matcher/matcher.go
@@ -52,6 +52,8 @@ func NewMatcher(ignoredUsers *bot.UserList) (m *Matcher) {
 			"gigapress", "gigafactory", "openai", "boring", "hyperloop", "solarcity", "neuralink", "sls", "nasa_sls", "ula", "vulcan", "artemis", "rogozin", "virgingalactic", "virgin galactic", "virgin orbit", "virginorbit", "blueorigin", "boeing", "starliner", "soyuz", "orion",
 
 			"covid", "corona", "pandemie", "pandemic", "impfung", "vaccine", "vax",
+
+			"jesus", "god", "gott", "der herr",
 		},
 	}
 
